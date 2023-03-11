c5784842579d65c98573e93a552c09324c97abda
kenndave <dave.bahana@gmail.com>
2023-03-11 14:04:52 +0700

    Push lagi

--- src/classes/implements/Card/unoAbilityCard.cpp
+++ src/classes/implements/Card/unoAbilityCard.cpp
@@ -3,4 +3,14 @@
 
 using namespace std;
 
-UnoAbilityCard::UnoAbilityCard(){}
+UnoAbilityCard::UnoAbilityCard(): AbilityCard(){
+    this->color = "";
+}
+
+UnoAbilityCard::UnoAbilityCard(string color): AbilityCard(){
+    this->color = color;
+}
+
+UnoAbilityCard::UnoAbilityCard(const UnoAbilityCard& other){
+    this->color = other.color;
+}
