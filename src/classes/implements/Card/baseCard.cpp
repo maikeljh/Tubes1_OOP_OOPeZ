0218fcd3e5d68638b474b3d8ef73e26ab08a0b75
kenndave <dave.bahana@gmail.com>
2023-03-11 13:24:33 +0700

    Uno

---
+++ src/classes/implements/Card/baseCard.cpp
@@ -0,0 +1,6 @@
+#include "../../headers/BaseCard/baseCard.hpp"
+#include <iostream>
+
+using namespace std;
+
+BaseCard::BaseCard(){}
---
+++ src/classes/implements/Card/unoAbilityCard.cpp
@@ -0,0 +1,6 @@
+#include "../../headers/Value/Card/unoAbilityCard.hpp"
+#include <iostream>
+
+using namespace std;
+
+UnoAbilityCard::UnoAbilityCard(){}
