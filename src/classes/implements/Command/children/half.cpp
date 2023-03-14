94fd88967539c4e84ad91af003dc2f5f8689f5da
arleenchr <16521172@mahasiswa.itb.ac.id>
2023-03-14 23:28:26 +0700

    fix print half quarter

--- src/classes/implements/Command/children/half.cpp
+++ src/classes/implements/Command/children/half.cpp
@@ -8,12 +8,13 @@ Half::Half():Command(){
 
 void Half::executeAction(CandyGame& Game){
     cout << endl << Game.getPlayer(0).getNickname() << " melakukan HALF! Poin hadiah";
-    Game.setPoint(Game.getPoint()*0.5);
+    int prevPoint = Game.getPoint();
+    Game.setPoint(prevPoint*0.5);
     if(Game.getPoint() == 0){
         Game.setPoint(1);
         cout << " tetap bernilai " << Game.getPoint() << "!" << endl;
     } else {
-        cout << " turun dari " << Game.getPoint() << " menjadi " << Game.getPoint() << "!" << endl;
+        cout << " turun dari " << prevPoint << " menjadi " << Game.getPoint() << "!" << endl;
     }
     Game.setValid(true);
 }
